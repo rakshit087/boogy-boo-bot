49f4d4e5d6eb4514e72f8a04c94133478b8b948a
Rakshit <rakshit087@gmail.com>
2022-05-14 12:19:16 +0530

    add story on image

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFont, ImageDraw
 import textwrap
 
 
-def create_image():
+def create_image(story):
     # Blank Image with White Border
     img = Image.new('RGB', (1280, 1280), color='black')
     border = Image.new('RGB', (1240, 1240), color='white')
@@ -17,3 +17,11 @@ def create_image():
     draw = ImageDraw.Draw(img)
     draw.text((int((655)-(150)), 1280-55), "@BOOGY.BOO",
               font=font, fill=(255, 255, 255))
+    # Adding Story
+    font = ImageFont.truetype('./fonts/Poppins-Regular.ttf', size=34)
+    story_lines = textwrap.wrap(story, width=60)
+    y_text = int((1280/2)-(len(story_lines)*25))
+    for line in story_lines:
+        draw.text((1280/2, y_text), line, font=font, fill=(255,255,255), anchor="ma")
+        y_text += 50
+    img.save('output.png')
