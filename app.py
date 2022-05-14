62248df17ef96f764bf3e049fab2d44fc0bc8dae
Rakshit <rakshit087@gmail.com>
2022-05-14 12:16:16 +0530

    Added Branding

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
 
+
 def create_image():
     # Blank Image with White Border
-    img = Image.new('RGB', (1280, 1280), color = 'black')
-    border = Image.new('RGB', (1240, 1240), color = 'white')
-    img.paste(border, (20,20))
-    border = Image.new('RGB', (1200, 1200), color = 'black')
-    img.paste(border, (40,40))
+    img = Image.new('RGB', (1280, 1280), color='black')
+    border = Image.new('RGB', (1240, 1240), color='white')
+    img.paste(border, (20, 20))
+    border = Image.new('RGB', (1200, 1200), color='black')
+    img.paste(border, (40, 40))
+    # Adding small rectangle at bottom
+    rect = Image.new('RGB', (300, 40), color='black')
+    img.paste(rect, (int((1280/2)-(150)), 1280-40))
+    # Adding Branding
+    font = ImageFont.truetype('./fonts/Poppins-Bold.ttf', size=36)
+    draw = ImageDraw.Draw(img)
+    draw.text((int((655)-(150)), 1280-55), "@BOOGY.BOO",
+              font=font, fill=(255, 255, 255))
